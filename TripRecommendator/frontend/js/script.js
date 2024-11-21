04dc66600ccf5dcdf7b5bc1a597e88de6897f374
Alba <albagar4@student.42malaga.com>
2024-11-21 15:16:34 +0100

    Form page done

--- TripRecommendator/frontend/js/script.js
+++ TripRecommendator/frontend/js/script.js
@@ -1,8 +1,14 @@
 let initPage = document.querySelector('.init-page');
 let formPage = document.querySelector('.form-page');
 let formOption = document.getElementById('form-option');
+let backButton = document.getElementById('back-btn');
 
 formOption.addEventListener('click', () => {
 	initPage.style.display = 'none';
 	formPage.style.display = 'block';
+});
+
+backButton.addEventListener('click', () => {
+	initPage.style.display = 'block';
+	formPage.style.display = 'none';
 });
